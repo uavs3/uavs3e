59f5a0126855fbc82f6a1016cfef3fc03b83a672
hanbingjie <hanbj@pku.edu.cn>
2020-08-21 14:56:53 +0800

    update CMake file

--- version.sh
+++ version.sh
@@ -8,6 +8,13 @@
 
 # get version of remote and local repository
 
+shell_dir=""
+if [ ! -n "$1" ]; then
+    shell_dir="."
+else
+    shell_dir=$1
+fi
+
 VER_R=`git rev-list origin/master | sort | wc -l | gawk '{print $1}'`
 VER_L=`git rev-list HEAD | sort | wc -l | gawk '{print $1}'`
 VER_SHA1=`git log -n 1 | head -n 1 | cut -d ' ' -f 2`
@@ -17,23 +24,23 @@ minor_version="2"
 type_version="release"
 
 # generate the file version.h
-echo "// ==========================================================================="  > version.h
-echo "// version.h"                                                                   >> version.h
-echo "// - collection of version numbers"                                             >> version.h
-echo "// ===========================================================================" >> version.h
-echo ""                                                                               >> version.h
-echo "#ifndef __VERSION_H__"                                                          >> version.h
-echo "#define __VERSION_H__"                                                          >> version.h
-echo ""                                                                               >> version.h
-echo "#define VER_MAJOR  $major_version                // major version number"       >> version.h
-echo "#define VER_MINOR  $minor_version                // minor version number"       >> version.h
-echo "#define VER_BUILD  $VER_L             // build number"                          >> version.h
-echo ""                                                                               >> version.h
-echo "#define VERSION_TYPE \"$type_version\""                                         >> version.h
-echo "#define VERSION_STR  \"$major_version.$minor_version.$VER_L\""                  >> version.h
-echo "#define VERSION_SHA1 \"$VER_SHA1\""                                             >> version.h
-echo ""                                                                               >> version.h
-echo "#endif // __VERSION_H__"                                                        >> version.h
+echo "// ==========================================================================="  > $shell_dir/version.h
+echo "// version.h"                                                                   >> $shell_dir/version.h
+echo "// - collection of version numbers"                                             >> $shell_dir/version.h
+echo "// ===========================================================================" >> $shell_dir/version.h
+echo ""                                                                               >> $shell_dir/version.h
+echo "#ifndef __VERSION_H__"                                                          >> $shell_dir/version.h
+echo "#define __VERSION_H__"                                                          >> $shell_dir/version.h
+echo ""                                                                               >> $shell_dir/version.h
+echo "#define VER_MAJOR  $major_version                // major version number"       >> $shell_dir/version.h
+echo "#define VER_MINOR  $minor_version                // minor version number"       >> $shell_dir/version.h
+echo "#define VER_BUILD  $VER_L             // build number"                          >> $shell_dir/version.h
+echo ""                                                                               >> $shell_dir/version.h
+echo "#define VERSION_TYPE \"$type_version\""                                         >> $shell_dir/version.h
+echo "#define VERSION_STR  \"$major_version.$minor_version.$VER_L\""                  >> $shell_dir/version.h
+echo "#define VERSION_SHA1 \"$VER_SHA1\""                                             >> $shell_dir/version.h
+echo ""                                                                               >> $shell_dir/version.h
+echo "#endif // __VERSION_H__"                                                        >> $shell_dir/version.h
 
 # show version informations
 echo "  "
