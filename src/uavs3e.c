3301562a31700929afa89bb935f4fd99dd1f3eb2
zywang <wangzhenyu@pkusz.edu.cn>
2020-08-23 12:33:38 +0800

    限制bt最大为64x64

--- src/uavs3e.c
+++ src/uavs3e.c
@@ -202,6 +202,14 @@ static void pic_enc_free(enc_pic_t *ep)
 
 static int refine_input_cfg(enc_cfg_t *param, enc_cfg_t *cfg_org)
 {
+    /***************************************************************************/
+    /*              refine config by speed_level                               */
+    /***************************************************************************/
+    if (SPEED_LEVEL(1, cfg_org->speed_level)) {
+        cfg_org->max_bt_size = 64;
+    }
+    /***************************************************************************/
+
     com_mcpy(param, cfg_org, sizeof(enc_cfg_t));
 
     if (param->bit_depth_internal == 0) {
@@ -1321,10 +1329,10 @@ void *uavs3e_create(enc_cfg_t *cfg, int *err)
     info->intra_rmd                = SPEED_LEVEL(1, h->cfg.speed_level);
     info->skip_split_P1            = SPEED_LEVEL(1, h->cfg.speed_level);
 
-    info->skip_adaptive_num_rdo    = SPEED_LEVEL(2, h->cfg.speed_level);
-    info->skip_adaptive_num_rdo_P1 = SPEED_LEVEL(3, h->cfg.speed_level);
     info->inter_adaptive_num_rdo   = SPEED_LEVEL(1, h->cfg.speed_level);
     info->inter_uni_same_ref       = SPEED_LEVEL(1, h->cfg.speed_level);
+    info->skip_adaptive_num_rdo    = SPEED_LEVEL(2, h->cfg.speed_level);
+    info->skip_adaptive_num_rdo_P1 = SPEED_LEVEL(3, h->cfg.speed_level);
 
     return h;
 }
